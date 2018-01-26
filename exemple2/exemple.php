cff2d4e786289ab7a9691a7d3f18d9bd66b9a26c
davi <davisamuel@inforgeneses.com.br>
2018-01-26 16:35:15 -0300

    modelo2

---
+++ exemple2/exemple.php
@@ -0,0 +1,48 @@
+<?php
+require_once '../constants.php';
+require_once DOMAIN_PATH_OMR.'src/OpenOMR/PaperSheet/PaperSheet.php';
+require_once DOMAIN_PATH_OMR.'src/OpenOMR/PaperSheet/Field.php';
+require_once DOMAIN_PATH_OMR.'src/OpenOMR/PaperSheet/Mark.php';
+require_once DOMAIN_PATH_OMR.'src/OpenOMR/Reader/Reader.php';
+
+$paper = new PaperSheet(38, 54);
+
+// identificador utilizado para cada questao
+$identificadorUnico = 1;
+// quantidade de colunas das respostas
+$qtdDeColunasRespostas = 6;
+// quantidade de respostas em cada coluna
+$qtdDeRespostas = 5;
+$letrasRespostas = array('A','B','C','D','E');
+
+// distancia entre o ponto inicial e final das questoes
+$pontoInicial = 49;
+$pontoFinal = 53;
+// ponto da numeracao da questao - iremos pular a posicao do numero da questao. Utilizaremos somente os pontos das respostas
+$posicaoAtualDaLeitura = 0;
+
+// percorrendo as colunas para pegar as respostas
+for ($colunaAtual = 1; $colunaAtual <= $qtdDeColunasRespostas; $colunaAtual++) {
+	// o primeiro ponto eh uma questao. pulando ele. Cada iteracao informa que acabaram as respostas. pulando o ponto da questao.
+	$posicaoAtualDaLeitura++;
+
+	// percorrendo os pontos na vertical ( eixo y )
+	for ($pontoAtual = $pontoInicial; $pontoAtual <= $pontoFinal; $pontoAtual++) {
+		// criando a referencia para um campo com id unico. valores de 1 a 9 recebem um zero a esquerda.
+		$field = new Field(str_pad($identificadorUnico, 2, '0', STR_PAD_LEFT));
+
+		// percorrendo os pontos na horizontal( eixo x )
+		for ($respostaAtual = 0; $respostaAtual < $qtdDeRespostas; $respostaAtual++) { 
+			$field->addMark(new Mark($pontoAtual, $posicaoAtualDaLeitura, $letrasRespostas[$respostaAtual]));
+		}
+
+		$paper->addField($field);
+
+		$identificadorUnico++;
+		// incrementando o ponto da resposta
+		$posicaoAtualDaLeitura++;
+	}
+}
+
+$reader = new Reader('openomr.jpg', $paper, 4);
+var_dump($reader->getResults());
